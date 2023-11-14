378a90c07adfcb0a9e4e19fd1826d134c09ce0e6
박성진 <psj2716@gmail.com>
2023-11-14 11:35:43 +0900

    🔨 [FEATURE] 상호작용 스크립트 추가

---
+++ src/script/map/interactions.ts
@@ -0,0 +1,26 @@
+/**
+ * 상호작용 모듈
+ *
+ * @author RWB
+ * @since 2023.11.14 Tue 11:27:59
+ */
+
+import { clickStyle, hoverStyle } from '@gis-dev/script/map/style';
+import { click, pointerMove } from 'ol/events/condition';
+import Select from 'ol/interaction/Select';
+import Style from 'ol/style/Style';
+
+const wfsHoverSelect = new Select({
+	condition: pointerMove,
+	style: (feature): Style => hoverStyle(feature, 'buld_nm')
+});
+
+const wfsClickSelect = new Select({
+	condition: click,
+	style: (feature): Style => clickStyle(feature, 'buld_nm')
+});
+
+export const selects = {
+	wfsClickSelect,
+	wfsHoverSelect
+};
