8a0505cda57980e13218bb00426a98c1b2f6d3ec
박성진 <psj2716@gmail.com>
2023-11-14 16:00:30 +0900

    ✨ [fEATURE] 신규 컴포넌트 추가 - WFS 팝업 페이지 - WFS 팝업 박스 - 맵 팝업

---
+++ src/app/wfs-popup/page.tsx
@@ -0,0 +1,21 @@
+/**
+ * WFS 팝업 페이지 컴포넌트
+ *
+ * @author RWB
+ * @since 2023.11.14 Tue 13:11:14
+ */
+
+import WFSPopupBox from '@gis-dev/components/organism/page/WFSPopupBox';
+import { ReactNode } from 'react';
+
+/**
+ * WFS 팝업 페이지 컴포넌트 반환 메서드
+ *
+ * @returns {ReactNode} ReactNode
+ */
+export default function WFSPopupPage(): ReactNode
+{
+	return (
+		<WFSPopupBox />
+	);
+}
---
+++ src/components/organism/page/WFSPopupBox/index.ts
@@ -0,0 +1,10 @@
+/**
+ * WFS 팝업 박스 organism 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2023.11.14 Tue 13:10:50
+ */
+
+import WFSPopupBox from './WFSPopupBox';
+
+export default WFSPopupBox;
