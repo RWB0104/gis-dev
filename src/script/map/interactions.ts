bbc88297296bcd723a6f21acdd5c8750f9fef4ff
박성진 <psj2716@gmail.com>
2023-11-14 16:01:31 +0900

    💡 [UPDATE] 주석 누락 추가

--- src/script/map/interactions.ts
+++ src/script/map/interactions.ts
@@ -10,11 +10,13 @@ import { click, pointerMove } from 'ol/events/condition';
 import Select from 'ol/interaction/Select';
 import Style from 'ol/style/Style';
 
+// WFS 호버 이벤트
 const wfsHoverSelect = new Select({
 	condition: pointerMove,
 	style: (feature): Style => hoverStyle(feature, 'buld_nm')
 });
 
+// WFS 클릭 이벤트
 const wfsClickSelect = new Select({
 	condition: click,
 	style: (feature): Style => clickStyle(feature, 'buld_nm')
