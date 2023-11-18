4faa9ada5970de8a219c12529bc55d113f948993
RWB0104 <psj2716@mensakorea.org>
2023-11-19 02:31:38 +0900

    👽️ [UPDATE] 트랜잭션 수정 API 추가

--- src/api/wfs.ts
+++ src/api/wfs.ts
@@ -61,11 +61,28 @@ export interface GeoJsonProps
 
 export interface PostFeatureProps
 {
+	/**
+	 * 빌딩명
+	 */
 	name: string;
 
+	/**
+	 * 빌딩 주소
+	 */
 	address: string;
 
+	/**
+	 * 피쳐
+	 */
 	features: Polygon[];
+}
+
+export interface PatchFeatureProps extends PostFeatureProps
+{
+	/**
+	 * 아이디
+	 */
+	id: string | number;
 }
 
 /**
@@ -163,6 +180,96 @@ export function usePostFeature(options?: ApiMutationOptions<Document, Response, 
 			return document;
 		},
 		mutationKey: [ 'wfs', 'usePostFeature' ],
+		...options
+	});
+}
+
+/**
+ * 피쳐 수정 비동기 메서드
+ *
+ * @param {ApiMutationOptions} options: ApiMutationOptions 객체
+ *
+ * @returns {UseMutationResult} UseMutationResult 객체
+ */
+export function useUpdateFeature(options?: ApiMutationOptions<Document, Response, PatchFeatureProps>): UseMutationResult<Document, Response, PatchFeatureProps>
+{
+	return useMutation({
+		mutationFn: async (data) =>
+		{
+			let geom = '';
+
+			// 멀티 폴리곤일 경우
+			if (data.features.length > 1)
+			{
+				const polygons = data.features.map((i) =>
+				{
+					const polygon = getPolygonXml(i).trim();
+
+					return `
+					<gml:polygonMember>
+						${polygon}
+					</gml:polygonMember>
+					`;
+				}).join('\n');
+
+				geom = `
+				<gml:MultiPolygon xmlns="http://www.opengis.net/gml" srsName="EPSG:3857">
+					${polygons}
+				</gml:MultiPolygon>
+				`;
+			}
+
+			// 아닐 경우
+			else
+			{
+				geom = getPolygonXml(data.features[0]);
+			}
+
+			const request = `
+			<wfs:Transaction
+				xmlns:wfs="http://www.opengis.net/wfs"
+				xmlns:gml="http://www.opengis.net/gml"
+				xmlns:ogc="http://www.opengis.net/ogc"
+				xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+				service="WFS"
+				version="1.1.0"
+				xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.0.0/WFS-transaction.xsd">
+				<wfs:Update typeName="buld_test">
+					<wfs:Property>
+						<wfs:Name>name</wfs:Name>
+						<wfs:Value>${data.name}</wfs:Value>
+					</wfs:Property>
+
+					<wfs:Property>
+						<wfs:Name>address</wfs:Name>
+						<wfs:Value>${data.name}</wfs:Value>
+					</wfs:Property>
+
+					<wfs:Property>
+						<wfs:Name>SHAPE</wfs:Name>
+						<wfs:Value>
+							${geom}
+						</wfs:Value>
+					</wfs:Property>
+
+					<ogc:Filter>
+						<ogc:FeatureId fid="${data.id}" />
+					</ogc:Filter>
+				</wfs:Update>
+			</wfs:Transaction>
+			`;
+
+			const response = await fetch(`${API_BASE_URL}/wfs`, {
+				body: request,
+				method: 'POST'
+			});
+
+			const xml = await response.text();
+			const document = new DOMParser().parseFromString(xml, 'text/xml');
+
+			return document;
+		},
+		mutationKey: [ 'wfs', 'useUpdateFeature' ],
 		...options
 	});
 }
