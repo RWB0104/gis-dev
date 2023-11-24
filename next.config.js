2af5a487a4e6eece1f44d24b01632235c118994e
RWB0104 <psj2716@mensakorea.org>
2023-11-24 14:31:02 +0900

    🔧 [UPDATE] PWA 적용

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development'
+});
+
+const nextConfig = withPWA({
 	basePath: '/gis-dev',
 	output: 'export'
-};
+});
 
 module.exports = nextConfig;
